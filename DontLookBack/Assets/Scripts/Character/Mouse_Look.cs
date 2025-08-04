99b40eb167d6ecf67891e5cd1c4a7bb9f4648776
Sara Mustonen <mustonenmedia@gmail.com>
2025-08-04 19:33:02 +0300

    working with movement

---
+++ DontLookBack/Assets/Scripts/Character/Mouse_Look.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+
+        void Update()
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+            yRotation += mouseX;
+
+            xRotation -=mouseY;
+        }
+    }
+}
